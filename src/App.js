cebbbeafb556c1b7bf8182bf06d44abb52301dac
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-09-08 22:25:34 +1000

    main: create sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
