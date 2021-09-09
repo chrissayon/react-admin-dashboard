7c50517c0548d80163a83ab8accb8205896af682
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-09-09 22:49:53 +1000

    main: add react chart

---
+++ src/dummyData.js
@@ -0,0 +1,46 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000
+    },
+    {
+      name: 'Apr',
+      "Active User": 2000
+    },
+    {
+      name: 'May',
+      "Active User": 2780
+    },
+    {
+      name: 'Jun',
+      "Active User": 1890
+    },
+    {
+      name: 'Jul',
+      "Active User": 2390
+    },
+    {
+      name: 'Aug',
+      "Active User": 3490
+    },
+    {
+      name: 'Sep',
+      "Active User": 3490
+    },
+    {
+      name: 'Oct',
+      "Active User": 3490
+    },
+    {
+      name: 'Nov',
+      "Active User": 3490
+    },
+    {
+      name: 'Dev',
+      "Active User": 3490
+    }
+];
